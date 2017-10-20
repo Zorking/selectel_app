f955b11c4667c412e1c4225ac8a2d69b82fb0e73
slavrov <slavrov93@gmail.com>
2017-10-21 02:11:55 +0300

    init

--- api/urls.py
+++ api/urls.py
@@ -4,7 +4,7 @@ from django.views.generic import TemplateView
 from api import views
 
 urlpatterns = [
-    url(r'register/$', TemplateView.as_view(), name='index'),
+    url(r'/$', TemplateView.as_view(), name='index'),
     url(r'register/$', views.RegisterView.as_view(), name='register'),
     url(r'tickets/$', views.TicketsView.as_view(), name='register'),
 ]

676d10584d1a3a1aef982206e3c1ebd5acd0e130
slavrov <slavrov93@gmail.com>
2017-10-21 16:59:00 +0300

    init

--- api/models.py
+++ api/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 
 class User(models.Model):
-    uid = models.IntegerField()
-    sid = models.CharField(max_length=255)
     vk_id = models.IntegerField(primary_key=True)
+    api_token = models.CharField(max_length=255)
+    exp_token = models.CharField(max_length=255)

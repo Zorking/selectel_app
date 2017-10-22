be4ba0b3dd0fe3a1dbbbba6367bb22fc245ddf02
slavrov <slavrov93@gmail.com>
2017-10-22 03:59:13 +0300

    servers

--- selectel_app/settings.py
+++ selectel_app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '%_!gv7!t2s2l4(^m^fz$5zu&wutxy_2=jpadj+@ctk+2y$ypy-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['itjustworks.me', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -124,6 +124,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 CORS_ORIGIN_ALLOW_ALL = True
 TEMPLATE_DIRS = (os.path.join(BASE_DIR,  'templates'),)
+X_FRAME_OPTIONS = 'ALLOW'
 
 try:
     from local_settings import *
